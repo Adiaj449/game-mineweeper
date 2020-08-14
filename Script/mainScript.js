48f6b583d87065860df8a9e1f57a483a54c4cbe4
Ajinkya Algonda <ajinkyasoftech449@gmail.com>
2020-08-14 19:02:42 +0530

    Addtion of time and passing Username using Location function

--- Script/mainScript.js
+++ Script/mainScript.js
@@ -1,8 +1,85 @@
-function getString()
+function loadMainScreen()
+{
+    var body = document.getElementsByTagName('body');
+    document.body.append(createHeader());
+    document.body.append(createAudio());
+}
+function createHeader()
+{
+    var divCont = document.createElement('div');
+    divCont.setAttribute('class','mainBanner');
+
+    var row = document.createElement('div');
+    //row.setAttribute('class','row');
+    divCont.append(row);
+
+    var col1 = document.createElement('div');
+    col1.setAttribute('class','mainHeading');
+    
+    var img = "<a onclick="+"togglePlay()>"+"<img style="+"color:'white';"+" src="+"Images/volume.png"+" height = 40px></a>";
+    var btn = "<button type="+"button" +"onclick="+"startGame()" +"value="+"btnStart"+">Start Game</button>";
+    var count = 300;
+    col1.innerHTML = 
+    `<table style="width:100%">
+        <th>User : `+getUsername()+` </th>
+        <th>Time Remaining</th>
+        <th> Music</th>
+         
+        <tr>
+            <td id ="timeRemaining">
+                hello
+            </td>
+            <td>
+                `+ btn +`
+            </td>
+            <td>
+                `+img +`
+            </td>
+        </tr>
+    </table>`;
+    
+      
+    var count = 5;
+    setInterval(function(){
+        if(count == 0)
+        {
+            clearInterval(this);
+            
+        }
+        document.getElementById('timeRemaining').innerHTML= "<br>Remaining Time " + count + " Seconds";
+        count--;
+    },1000);
+
+    /*
+    var col1 = document.createElement('div');
+    col1.setAttribute('class','mainHeading');
+    col1.innerHTML = "UserName : "+getUsername();
+   
+    var soundBar = document.createElement('div');
+    soundBar.setAttribute('class','mainHeading soundBar');
+    soundBar.innerHTML= "<br><br><a onclick="+"togglePlay()>"+"<img style="+"color:'white';"+" src="+"Images/volume.png"+" height = 40px></a>";
+
+    var timeBar = document.createElement('div');
+    timeBar.setAttribute('class','timeBar');
+    
+    
+    */
+
+    
+    row.appendChild(col1);
+    //row.appendChild(soundBar);
+    //row.appendChild(timeBar);
+
+    divCont.appendChild(row);
+    return divCont;
+
+}
+ 
+function getUsername()
 {
     var query = window.location.href;
     var para = query.split('=');
     const userName = para[1];
-    alert(userName);
+    return userName;
 }
 
--- Script/script.js
+++ Script/script.js
@@ -33,7 +33,7 @@ function createPoster()
     
     var soundBar = document.createElement('div');
     soundBar.setAttribute('class','container soundBar');
-    soundBar.innerHTML= "<a onclick="+"togglePlay()>"+"<img style="+"color:'white';"+" src="+"Images/volume.png"+"></a>";
+    soundBar.innerHTML= "<a onclick="+"togglePlay()>"+"<img style="+"color:'white';"+" src="+"Images/volume.png"+" ></a>";
 
     container.appendChild(soundBar);
 
@@ -43,7 +43,7 @@ function createPoster()
 
     var col1 = document.createElement('div');
     col1.setAttribute('class','headingPoster');
-    col1.innerHTML ="<form action="+"mainPage.html"+" method="+"get"+"><center><h1>Welcome to World of MinesWheeper</h1><label for="+"userName"+">Enter Usernme : </label> <input id="+"userName"+ " name ="+"userName"+"type="+"text"+"value="+"Enter"+"> <br> <br><button class = "+"btn"+" type="+"submit"+"value="+"PlayGame"+">Play Game</button></center></form>";
+    col1.innerHTML ="<form action="+"mainPage.html"+" method="+"get"+"><center><h1>Welcome to World of MinesWheeper</h1><label for="+"userName"+">Enter Usernme : </label> <input id="+"userName"+ " name ="+"userName"+"type="+"text"+"value="+"Enter"+ " required> <br> <br><button class = "+"btn"+" type="+"submit"+"value="+"PlayGame"+">Play Game</button></center></form>";
 
     var col2 = document.createElement('div');
     col2.setAttribute('class','headingPoster');
